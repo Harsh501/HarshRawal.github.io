ae78e92c733ee4371876bc93fbc0df5bcd9c09a5
Harsh Rawal <harshrawal@Harshs-MacBook-Pro.local>
2026-01-03 18:16:22 -0800

    feat: integrate font awesome for social icons in nav

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,17 +1,20 @@
 export default {
   base: '/HarshRawal.github.io/', 
 
-  title: 'Harsh Rawal Blog',
+  title: 'Harsh Rawal',
   description: 'My personal blog and documentation.',
+  head: [
+    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css' }]
+  ],
 
   themeConfig: {
     nav: [
       {
         text: 'Connect',
         items: [
-          { text: 'GitHub', link: 'https://github.com/Harsh501' },
-          { text: 'LinkedIn', link: 'https://www.linkedin.com/in/harsh-rawal-1b9397102' },
-          { text: 'Chess.com', link: 'https://www.chess.com/member/hdr501'}
+          { text: '<i class="fa-brands fa-github"></i> GitHub', link: 'https://github.com/Harsh501' },
+          { text: '<i class="fa-brands fa-linkedin"></i> LinkedIn', link: 'https://www.linkedin.com/in/harsh-rawal-1b9397102' },
+          { text: '<i class="fa-solid fa-chess-pawn"></i> Chess.com', link: 'https://www.chess.com/member/hdr501'}
         ]
       }
     ]
